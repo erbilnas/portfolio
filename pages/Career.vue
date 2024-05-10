9376f9fdf3aa5d866dafb4eebae70b2848bb2a9a
Erbil Nas <erbil.nas@trendyol.com>
2024-05-10 12:00:27 +0300

    feature: add card animation

--- pages/Career.vue
+++ pages/Career.vue
@@ -46,5 +46,18 @@ useHead({
     display: flex;
     justify-content: center;
   }
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+      transform: translateY(1rem);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+
+  animation: fadeIn 0.5s;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -51,6 +51,19 @@ main {
       );
       transition: box-shadow 0.3s ease;
     }
+
+    @keyframes fadeIn {
+      from {
+        opacity: 0;
+        transform: translateY(1rem);
+      }
+      to {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
+
+    animation: fadeIn 0.5s;
   }
 
   .summary-card {
