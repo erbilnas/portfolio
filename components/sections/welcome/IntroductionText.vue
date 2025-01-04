02b0c4a58b5a1a1d78a623203045e69566cbd7af
Erbil Nas <erbil.nas@trendyol.com>
2025-01-04 17:53:04 +0300

    feat(welcome): improve responsive of introduction text

--- components/sections/welcome/IntroductionText.vue
+++ components/sections/welcome/IntroductionText.vue
@@ -1,13 +1,11 @@
 <template>
-  <span class="text-6xl">
+  <span class="text-5xl sm:text-6xl">
     <TextHighlight
       class="rounded-xl bg-gradient-to-r from-violet-300 to-violet-50 px-4 py-2"
       text-end-color="hsl(var(--accent))"
     >
       It's me,
-      <span
-        class="bg-gradient-to-r from-violet-400 to-violet-600 bg-clip-text font-semibold text-transparent"
-      >
+      <span class="bg-violet-600 bg-clip-text font-semibold text-transparent">
         Erbil.
       </span>
     </TextHighlight>
